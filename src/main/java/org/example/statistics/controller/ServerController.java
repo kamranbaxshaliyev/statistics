7c1a73b862d8dc331268879fc8a9921bbd1ca318
kamranbx <kamranbaxshaliyev567@gmail.com>
2025-10-30 14:32:34 +0400

    init

---
+++ src/main/java/org/example/statistics/controller/ServerController.java
@@ -0,0 +1,5 @@
+package org.example.statistics.controller;
+
+public class ServerController
+{
+}
---
+++ src/test/java/org/example/statistics/StatisticsApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
